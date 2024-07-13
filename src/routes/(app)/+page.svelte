87dbeb204c6a4a43268b6249c5bee70f2753f027
Mwungere <mwungereelite56@gmail.com>
2024-07-13 16:01:32 +0200

    ninja gaming guides about grouping layouts

---
+++ src/routes/(app)/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+
+</script>
+
+
+<div class="index">
+  <h2>Welcome</h2>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum vitae,
+    nulla nemo a eaque repudiandae nam nostrum saepe ullam ratione, quisquam
+    totam architecto fugit sed repellat vero deleniti, quidem in harum impedit
+    quia molestias atque perspiciatis natus. Autem dignissimos consequuntur
+    veritatis? Ullam dolores quasi deleniti ducimus sit, perferendis eligendi
+    magni.
+  </p>
+  <a href="/guides">View Guides</a>
+  <a href="/about">About</a>
+</div>
+
+<style>
+    .index{
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
---
+++ src/routes/(app)/about/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+
+</script>
+<div class="about">
+  <h2>About us</h2>
+  <p>
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint accusamus,
+    qui perferendis ipsam enim earum fugit odit quis inventore architecto sed
+    vitae sapiente laborum quae facilis eos nemo consectetur harum corporis.
+    Iusto nostrum et natus ipsam provident vel eaque excepturi amet autem
+    pariatur temporibus consectetur quam, incidunt sed exercitationem est.
+  </p>
+  <a href="/guides">View Guides</a>
+  <a href="/">Home</a>
+</div>
+<style>
+    .about{
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
